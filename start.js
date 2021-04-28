922f29ee7eacf62daae0542b1b40bc6bfccb6a86
dci <rudeshdhote@gmail.com>
2021-04-28 22:32:52 +0200

    initial commit

--- start.js
+++ start.js
@@ -15,3 +15,50 @@ const userDataDump = [
   "D,Cheesedude51,1951,Users",
   "E,cron,,System",
 ];
+
+var getInitials = (string) => {
+  var names = string.split(' '),
+      initials = names[0].substring(0, 1).toUpperCase();
+  
+  if (names.length > 1) {
+      initials += names[names.length - 1].substring(0, 1).toUpperCase();
+  }
+  return initials;
+};
+
+let userDate = (arrayData) => {
+
+  const arrayOfUserData = [];
+  for (let i = 0; i < arrayData.length; i++){
+  
+     arrayOfUserData[i] = arrayData[i].split(",");
+}
+
+for (let i = 0; i < arrayOfUserData.length; i++) {
+
+  
+  const element = arrayOfUserData[i];
+
+  const objectData = {
+
+id:element[0],
+fullName: element[1],
+initial:getInitials(element[1]),
+birthYear: element[2]?element[2]:"unknown",
+departments: element[3].split("|"),
+
+
+
+  };
+
+  console.log(objectData);
+}
+
+
+
+}
+userDate(userDataDump)
+
+
+
+
